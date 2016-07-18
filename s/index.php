2194bd8a16dc10ff7c72989aae845f9519ee28f0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-07-19 00:38:16 +0200

    basic page layout

--- s/index.php
+++ s/index.php
@@ -3,4 +3,40 @@
 require_once('config.php');
 require_once('backend/main.php');
 
-echo 'Hallo, '.htmlspecialchars($_REQUEST['page']).'!';
+
+class Frame {
+	
+	function writeHtmlHeader($title) {
+		?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset='UTF-8'">
+	<link rel="stylesheet" href="/s/frontend/thurniwiki-00001.css">
+	<title><?php echo htmlspecialchars($title);?></title>
+</head>
+<body>
+<div class="frame">
+<header>
+	<img class="logo" src="s/frontend/logo.jpg">
+	<!-- <div>Thurnithistraße 20</div> -->
+	<h1><?php echo htmlspecialchars($title);?></h1>
+	<a class="navbutton" href="/">Startseite</a> <a class="navbutton" href="#">Login</a>
+</header>
+<?php
+	}
+
+
+	function writeHtmlFooter() {
+?>
+</div>
+</body>
+</html>
+<?php
+	}
+}
+
+$frame = new Frame();
+$frame->writeHtmlHeader('Titel der Seite');
+echo '<section><h2>Hallo, '.htmlspecialchars($_REQUEST['page']).'</h2></section>';
+$frame->writeHtmlFooter();
