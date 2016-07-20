92051d67e26c6df73c69d72b3f941ff8d6dfa3a5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-07-21 01:33:34 +0200

    edit link

--- s/backend/action/action.php
+++ s/backend/action/action.php
@@ -24,6 +24,7 @@ class Action {
 	echo '<h1>'.htmlspecialchars(CONFIG_SITE_TITLE).'</h1>';
 	echo '<a class="navbutton" href="/">Startseite</a>';
 	if (isset($session['accountId'])) {
+		echo '<a class="navbutton" href="?action=edit">Bearbeiten</a>';
 		echo '<a class="navbutton" href="?action=logout">Logout</a>';
 	} else {
 		echo '<a class="navbutton" href="?action=login">Login</a>';
